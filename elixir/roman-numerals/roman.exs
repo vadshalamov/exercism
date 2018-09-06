3e87160cded71dc2f071ced99fbd2915f8e5cdb7
Vadim Shalamov <vadshalamov@gmail.com>
2018-09-06 19:02:09 +0200

    small rename

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,5 +1,5 @@
 defmodule Roman do
-  @number_as_roman [
+  @arabic_as_roman [
     {1000, "M"},
     {900, "CM"},
     {500, "D"},
@@ -19,12 +19,12 @@ defmodule Roman do
   """
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
-    do_numerals(@number_as_roman, number)
+    do_numerals(@arabic_as_roman, number)
   end
 
   defp do_numerals([], _), do: ""
-  defp do_numerals([{arabic, roman} | _] = numbers, number) when number >= arabic,
-    do: roman <> do_numerals(numbers, number - arabic)
+  defp do_numerals([{arabic, roman} | _] = list, number) when number >= arabic,
+    do: roman <> do_numerals(list, number - arabic)
 
   defp do_numerals([_ | tail], number), do: do_numerals(tail, number)
 end
