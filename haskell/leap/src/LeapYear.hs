3da305a18b6970b04986838a46b14fc9d20611f7
Vadim Shalamov <vadshalamov@gmail.com>
2018-10-05 14:02:47 +0300

    refacotr leap year haskell task

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,7 +2,10 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | rem year 400 == 0 = True
-  | rem year 100 == 0 = False
-  | rem year 4 == 0 = True
+  | year `isDivisibleBy` 400 = True
+  | year `isDivisibleBy` 100 = False
+  | year `isDivisibleBy` 4   = True
   | otherwise = False
+
+isDivisibleBy :: Integer -> Integer -> Bool
+isDivisibleBy a b = a `rem` b == 0
