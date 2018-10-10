787792644d49d0b6e0e738e7e2b4f7f1500f819b
Vadim Shalamov <vadshalamov@gmail.com>
2018-10-10 12:13:20 +0500

    refactor markdown elixir task

--- elixir/markdown/markdown.exs
+++ elixir/markdown/markdown.exs
@@ -1,4 +1,9 @@
 defmodule Markdown do
+  @bold_text_prefix_regex ~r/^#{"__"}{1}/
+  @bold_text_suffix_regex ~r/#{"__"}{1}$/
+  @italic_text_prefix_regex ~r/^[#{"_"}{1}][^#{"_"}+]/
+  @italic_text_suffix_regex ~r/[^#{"_"}{1}]/
+
   @doc """
     Parses a given string with Markdown syntax and returns the associated HTML for that string.
 
@@ -11,80 +16,75 @@ defmodule Markdown do
     "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
   """
   @spec parse(String.t()) :: String.t()
-  def parse(m) do
-    m
+  def parse(text) do
+    text
     |> String.split("\n")
     |> Enum.map(&process/1)
     |> Enum.join()
-    |> patch()
+    |> wrap_list_with_tags()
   end
 
-  defp process("*" <> _ = t), do: parse_list_md_level(t)
-  defp process("#" <> _ = t) do
-    t
+  defp process("*" <> _ = line), do: parse_list_line(line)
+  defp process("#" <> _ = line) do
+   line
     |> String.split()
-    |> parse_header_md_level()
+    |> parse_heading()
     |> enclose_with_header_tag()
   end
 
-  defp process(t) do
-    t
+  defp process(paragraph) do
+    paragraph
     |> String.split()
     |> join_words_with_tags()
     |> wrap_with_tag("p")
   end
 
-  defp parse_header_md_level([h | t]) do
-    md_level =
-      h
-      |> String.length()
-      |> to_string()
-
-    {md_level, Enum.join(t, " ")}
+  defp parse_heading([signs | line]) do
+    {String.length(signs), Enum.join(line, " ")}
   end
 
-  defp parse_list_md_level(l) do
-    l
+  defp parse_list_line(line) do
+    line
     |> String.trim_leading("* ")
     |> String.split()
     |> join_words_with_tags()
     |> wrap_with_tag("li")
   end
 
-  defp wrap_with_tag(str, tag), do: "<#{tag}>" <> str <> "</#{tag}>"
+  defp wrap_with_tag(text, tag), do: "<#{tag}>" <> text <> "</#{tag}>"
 
-  defp enclose_with_header_tag({hl, htl}), do: wrap_with_tag(htl, "h#{hl}")
+  defp enclose_with_header_tag({level, line}), do: wrap_with_tag(line, "h#{level}")
 
-  defp join_words_with_tags(t) do
-    t
-    |> Enum.map(&replace_md_with_tag/1)
+  defp join_words_with_tags(words) do
+    words
+    |> Enum.map(&replace_word_with_tag/1)
     |> Enum.join(" ")
   end
 
-  defp replace_md_with_tag(w) do
-    w
-    |> replace_prefix_md()
-    |> replace_suffix_md()
+  defp replace_word_with_tag(word) do
+    word
+    |> replace_prefix_with_tag()
+    |> replace_suffix_with_tag()
   end
 
-  defp replace_prefix_md(w) do
+  defp replace_prefix_with_tag(word) do
     cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
+      word =~ @bold_text_prefix_regex -> String.replace(word, @bold_text_prefix_regex, "<strong>", global: false)
+      word =~ @italic_text_prefix_regex -> String.replace(word, ~r/_/, "<em>", global: false)
+      true -> word
     end
   end
 
-  defp replace_suffix_md(w) do
+  defp replace_suffix_with_tag(word) do
     cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
+      word =~ @bold_text_suffix_regex -> String.replace(word, @bold_text_suffix_regex, "</strong>")
+      word =~ @italic_text_suffix_regex -> String.replace(word, ~r/_/, "</em>")
+      true -> word
     end
   end
 
-  defp patch(l) do
-    l
+  defp wrap_list_with_tags(text) do
+    text
     |> String.replace("<li>", "<ul>" <> "<li>", global: false)
     |> String.replace_suffix("</li>", "</li>" <> "</ul>")
   end
