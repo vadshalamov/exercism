7439c568f0fe983f7ede42a32ba0f669d489ae9d
Vadim Shalamov <vadshalamov@gmail.com>
2018-10-21 19:34:29 +0500

    complete nucleotide count task (haskell)

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -6,12 +6,25 @@ import qualified Data.Map as Map
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = Right nucleotideCountsMap
+nucleotideCounts xs =
+  countNucleotide xs nucleotideCountsMap
   where
     nucleotideCountsMap = Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
 
-valid :: Char -> Either String Nucleotide
-valid c = case c of
+countNucleotide :: String -> Map Nucleotide Int -> Either String (Map Nucleotide Int)
+countNucleotide "" nucleotideCountsMap = Right nucleotideCountsMap
+
+countNucleotide (x:xs) nucleotideCountsMap =
+  handleNucleotide (toNucleotide x) xs nucleotideCountsMap
+
+handleNucleotide :: Either String Nucleotide -> String -> Map Nucleotide Int -> Either String (Map Nucleotide Int)
+handleNucleotide (Right nucleotide) xs nucleotideCountsMap =
+  countNucleotide xs (Map.adjust (1 +) nucleotide nucleotideCountsMap)
+handleNucleotide (Left message) _ _ = Left message
+
+
+toNucleotide :: Char -> Either String Nucleotide
+toNucleotide c = case c of
   'A' -> Right A
   'C' -> Right C
   'G' -> Right G
